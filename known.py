766e350bad4502e6df1e7e7b677b70b93e7bae13
Vince Knight <vincent.knight@gmail.com>
2020-09-22 15:06:00 +0100

    Run black.

--- known.py
+++ known.py
@@ -1,18 +1,25 @@
-words = set((
-    "AttributeError",
-    "Jupyter",
-    "NameError",
-    "ax",
-    "bc",
-    "docstring",
-    "inv",
-    "jupyter",
-    "sym",
-    "sympy",
-    ))
+words = set(
+    (
+        "AttributeError",
+        "Jupyter",
+        "NameError",
+        "ax",
+        "bc",
+        "docstring",
+        "inv",
+        "jupyter",
+        "sym",
+        "sympy",
+    )
+)
 
 prose_exceptions = {
-        "assets/nbs/assessment/mock/main.ipynb": set((r"The matrix \\(D\\) is given by \\(D = \begin{pmatrix} a & 2 & 0\\ 3 & 1 & 2\\ 0 & -1 & 1\end{pmatrix}\\) where \\(a\ne 2\\).",))}
+    "assets/nbs/assessment/mock/main.ipynb": set(
+        (
+            r"The matrix \\(D\\) is given by \\(D = \begin{pmatrix} a & 2 & 0\\ 3 & 1 & 2\\ 0 & -1 & 1\end{pmatrix}\\) where \\(a\ne 2\\).",
+        )
+    )
+}
 prose_suggestions_to_ignore = {
-        "assets/nbs/assessment/mock/main.ipynb": set(("typography.symbols.curly_quotes",)),
+    "assets/nbs/assessment/mock/main.ipynb": set(("typography.symbols.curly_quotes",)),
 }

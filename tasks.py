95b7c17fb0e1ea1a7dbd367fe1b1fb93ccfe07d0
Vince Knight <vincent.knight@gmail.com>
2020-09-22 17:29:57 +0100

    Fix by copying files (#174)
    * Revert "Try to keep root dir. (#173)"
    
    This reverts commit 5491acc41490cba5c36450701c6d52fd2f10afd4.
    
    * Copy assets directory.

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 import pathlib
+import shutil
 import subprocess
 import sys
 import tempfile
@@ -83,8 +84,10 @@ def build(
             "author": author,
         },
     )
-    out_file = pathlib.Path("index.html")
-    out_file.write_text(html)
+    out_dir = pathlib.Path("_build")
+    out_dir.mkdir(exist_ok=True)
+    (out_dir / "index.html").write_text(html)
+    shutil.copytree("assets", out_dir / "assets")
 
 
 @task

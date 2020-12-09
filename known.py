d7d574b3ec6a2dda94748318a75b6af9f672f040
Vince Knight <vincent.knight@gmail.com>
2020-12-09 12:08:14 +0000

    Mark mock (#214)
    * Write email script.
    
    * Modify checks in assignement.
    
    * Use asseertions instead of prints.
    
    * Tweak check for one question.
    
    * Run black.
    
    * Add known exceptions for prose.

--- known.py
+++ known.py
@@ -47,12 +47,20 @@ prose_exceptions = {
             r"The matrix \\(D\\) is given by \\(D = \begin{pmatrix} a & 2 & 0\\ 3 & 1 & 2\\ 0 & -1 & 1\end{pmatrix}\\) where \\(a\ne 2\\).",
         )
     ),
+    "assets/nbs/assessment/mock/solution.ipynb": set(
+        (
+            r"The matrix \\(D\\) is given by \\(D = \begin{pmatrix} a & 2 & 0\\ 3 & 1 & 2\\ 0 & -1 & 1\end{pmatrix}\\) where \\(a\ne 2\\).",
+        )
+    ),
 }
 prose_suggestions_to_ignore = {
     "assets/nbs/assessment/mock/main.ipynb": set(
         ("typography.symbols.curly_quotes", "garner.preferred_forms")
     ),
     "assets/nbs/assessment/mock/assignment.ipynb": set(
+        ("typography.symbols.curly_quotes", "garner.preferred_forms")
+    ),
+    "assets/nbs/assessment/mock/solution.ipynb": set(
         ("typography.symbols.curly_quotes", "garner.preferred_forms")
     ),
     "assets/nbs/assessment/example/main.ipynb": set(
